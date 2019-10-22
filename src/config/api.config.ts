94e629675fd530f805f0fc89a99a794df45fccbe
ggbzrr <ggbzrr@yahoo.com.br>
2019-10-22 01:05:42 -0300

    Comecando a implementar a autenticacao

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG={
-    baseurl: "http://localhost:8080",
+    baseUrl: "http://localhost:8080",
     bucketBaseUrl: "https://curso-spring-ionic-bezerra.s3-sa-east-1.amazonaws.com"
                     
 }
