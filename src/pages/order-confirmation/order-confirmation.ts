2e152b07756fbc82559b9ccbfeb619413505143d
ggbzrr <ggbzrr@yahoo.com.br>
2019-11-02 10:51:39 -0300

    Informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -21,6 +21,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -57,18 +58,27 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout(){
     this.pedidoService.insert(this.pedido)
       .subscribe(response =>{
         //limpar carrinho
         this.cartService.createOrClearCart();
         //testar se o location do pedido salvo se está funcionando
-        console.log(response.headers.get('location'));
+        this.codpedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if (error.status == 403){
           this.navCtrl.setRoot('HomePage'); 
         }
       })
+  }
+
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 }
